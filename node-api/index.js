94e313c99eaddc7479bd4a49083bcecd846befb6
satyendra yadav <satyendradv956@gmail.com>
2023-08-16 16:50:20 +0530

    api created

---
+++ node-api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+	res.send("NODE-API root")
+})
+
+app.listen(port,()=>{
+	console.log(`Node API running on port${port}`)
+})
